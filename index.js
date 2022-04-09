1ad97210011712b259f548f83adb111e67cd2426
salahqasm <salahqasm@outlook.com>
2022-04-10 01:21:59 +0300

    last

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 const express=require("express");
 const dotenv=require("dotenv");
 const resInfo=require('./routes/resInfo');
-const connectDB=require("./config/connect");
 const app=express();
 const PORT=process.env.PORT || 5000
 //load config
 dotenv.config({path:'./config/.env'})
+//db connection
+
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+client.connect((err)=>{
+    if(err){
+        console.log(err);
+        throw err;
+    }
+    console.log("connected");
+});
+
 
 app.use('/',resInfo)
-connectDB();
 
 
 app.listen(PORT,()=>{console.log(`SERVER RUNNING ON:${PORT}`);});
