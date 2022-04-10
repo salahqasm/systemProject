f1eb8d7d2141650a83933f3f39c8b015a230843c
salahqasm <salahqasm@outlook.com>
2022-04-11 00:25:23 +0300

    final modulization

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
 const express=require("express");
 const dotenv=require("dotenv");
 const client=require("./config/connect");
-const { path } = require("express/lib/application");
 const app=express();
 app.use(express.json())
-
 const PORT=process.env.PORT || 5000
 //load config
 dotenv.config({path:'./config/config.env'})
-//db connection
+
+
 
 
 app.use("/add",require("./routes/addRes"))
 
-app.get("/",(req,res)=>{
-    res.send(__dirname);
-})
 
 client.connect().then(() => {
         app.listen(PORT, () =>
